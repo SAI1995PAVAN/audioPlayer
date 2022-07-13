d3fc87912eaeb5e344186ccdca0f4cf2f1a703ab
SAI PAVAN <saipavankanduri@gmail.com>
2022-07-13 23:18:37 +0530

    forward backward functions added

--- src/Components/MusicPlayer/MusicPlayer.js
+++ src/Components/MusicPlayer/MusicPlayer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./MusicPlayer.css";
 import play from "./play.png";
-// import pause from "./pause2.png";
+import pause from "./pause2.png";
 import previous from "./previous.png";
 import next from "./next.png";
 import favouritesAfterClick from "./favourites.png";
@@ -10,24 +10,25 @@ import favouritesAfterClick from "./favourites.png";
 // import { SkipPreviousIcon, SkipNextIcon } from "@mui/icons-material";
 
 const MusicPlayer=(props)=> {
-  const{playSong,playPause,songImage}=props
+  const{playSong,handleplay,songImage,songUri,isPlaying,handleForward,handlePrevious}=props
     return (
       <div id="playSong">
         <div id="songImage">
           <img
             src={songImage}
             alt=""
-            
+            height="100%"
+            width="100%"
           />
         </div>
         <div id="playerFunctions">
-          <button id="playPreviousFunction" >
+          <button id="playPreviousFunction" onClick={handlePrevious} >
             <img src={previous} alt="" width="40px" height="40px" />
           </button>
-          <button id="playFunction" onClick={playPause}>
+          <a id="playFunction" onClick={handleplay} href={songUri}>
             <img src={play} alt="" width="50px" height="50px" />
-          </button>
-          <button id="playNextFunction">
+          </a>
+          <button id="playNextFunction" onClick={handleForward}>
             <img src={next} alt="" width="40px" height="40px" />
           </button> 
           <button id="favourites">
