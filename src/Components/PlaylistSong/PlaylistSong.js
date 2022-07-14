48038e1d159ee75466710cf061531c52caab21a8
SAI PAVAN <saipavankanduri@gmail.com>
2022-07-14 22:40:50 +0530

    favourites,delete,playlists updated

--- src/Components/PlaylistSong/PlaylistSong.js
+++ src/Components/PlaylistSong/PlaylistSong.js
@@ -8,17 +8,13 @@ const PlaylistSong = (props) => {
     onClick,
     handleFavourites,
     handleDeleteFromPlaylists,
-    isPlaying,
-    handlePlayPauseButtons,
   } = props;
   return (
     <div>
       <div className="eachsongPlaylist">
         <p>Track Name:{trackName}</p>
         <p>Artist Name:{artistName}</p>
-        <button onClick={onClick}>
-          {isPlaying === true ? "pause" : "play"}
-        </button>
+        <button onClick={onClick}>play</button>
         <button onClick={handleFavourites}>like</button>
         <button onClick={handleDeleteFromPlaylists}>Delete</button>
       </div>
